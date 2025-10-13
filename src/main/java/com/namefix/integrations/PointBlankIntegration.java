2d4e8c7d7430833b93b450fe9fc1caec4f4128d4
Namefix <41016273+Namefix@users.noreply.github.com>
2025-10-13 03:36:33 +0300

    Fixed VPB Sinytra incompatbility
    - Fixed a crash when Vic's Point Blank weapons are used in Deadeye (with Sinytra Connector on Forge)
    - Bump version

--- src/main/java/com/namefix/integrations/PointBlankIntegration.java
+++ src/main/java/com/namefix/integrations/PointBlankIntegration.java
@@ -1,5 +1,6 @@
 package com.namefix.integrations;
 
+import com.namefix.DeadeyeMod;
 import com.vicmatskiv.pointblank.client.GunClientState;
 import com.vicmatskiv.pointblank.item.FireMode;
 import com.vicmatskiv.pointblank.item.GunItem;
@@ -8,14 +9,36 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+import java.lang.reflect.Method;
 
 public class PointBlankIntegration {
     public static boolean isLoaded = false;
+    private static Method tryFireMethod = null;
+    private static boolean reflectionInitialized = false;
 
     public static void initialize() {
         isLoaded = true;
+        initializeReflection();
+    }
+
+    private static void initializeReflection() {
+        if (reflectionInitialized) return;
+        reflectionInitialized = true;
+
+        try {
+            // tryFire reflection
+            for (Method method : GunItem.class.getMethods()) {
+                if (method.getName().equals("tryFire") && method.getParameterCount() == 3) {
+                    tryFireMethod = method;
+                    tryFireMethod.setAccessible(true);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+			DeadeyeMod.LOGGER.error("Failed to initialize reflection for Point Blank: {}", e.getMessage());
+        }
     }
 
     public static boolean isItemGun(ItemStack stack) {
@@ -24,7 +47,14 @@ public class PointBlankIntegration {
     }
 
     public static void shootGun(GunItem gun, PlayerEntity player, Entity target) {
-        gun.tryFire(player, player.getMainHandStack(), target);
+        if (tryFireMethod == null) {
+            return;
+        }
+        try {
+            tryFireMethod.invoke(gun, player, player.getMainHandStack(), target);
+        } catch (Exception e) {
+            DeadeyeMod.LOGGER.error("Failed tryFire reflection for Point Blank: {}", e.getMessage());
+        }
     }
 
     public static boolean canMarkTargets(ItemStack item, int markCount) {
